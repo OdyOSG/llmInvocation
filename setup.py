856bba0b506188cb110d43687e45d9c2fd7f12f7
Gowtham Rao <rao@ohdsi.org>
2025-04-02 13:20:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,40 @@
 from setuptools import setup, find_packages
 
-# Standard Library Imports
-import os
-import re
-import time
-import xml.etree.ElementTree as ET
-
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name="llmInvocation",  
-    version="0.0.1",          
-    author="EPAM",
+    name="llmInvocation",
+    version="0.0.1",
+    author="",
     author_email="",
     description="LLM engine to process results of the PubMed Search",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/OdyOSG/llmInvocation",  
-    packages=find_packages(),  
+    url="https://github.com/OdyOSG/llmInvocation",
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-      "pyspark",
-      "numpy",
-      "pandas",
-      "langchain_openai",
-      "requests",
-      "XlsxWriter",
-      "IPython"
-    ]
+        "pyspark",
+        "numpy",
+        "pandas",
+        "langchain_openai",
+        "requests",
+        "XlsxWriter",
+        "IPython"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="LLM PubMed data-management AI",
+    license="MIT",
+    python_requires=">=3.8",
 )
